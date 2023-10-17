10c14354e415c9679fa07af8ae83573d844b70b9
Dan <0921pig@gmail.com>
2023-10-17 11:06:24 +0900

    svg import regacy
    v1.0.45
    
    v1.0.46

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ReactComponent as LoadingIcon } from '../../asset/image/ic_loading.svg';
 
 export interface IProps {
     className?: string
@@ -89,7 +88,16 @@ const Button: React.FC<IProps> = ({leadingIcon, trailingIcon, label, count = 0, 
             </div>
             {isLoading && (
                 <div className={'absolute w-[20px]'}>
-                    <LoadingIcon className={`w-[16px] h-[16px] animate-spin ${variantStyles.get(variant)?.loadingClassName}`} />
+                    {/*<LoadingIcon className={`w-[16px] h-[16px] animate-spin ${variantStyles.get(variant)?.loadingClassName}`} />*/}
+                    <svg width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         className={`hds-flight-icon--animation-loading w-[16px] h-[16px] animate-spin ${variantStyles.get(variant)?.loadingClassName}`}>
+                        <g fill-rule="evenodd" clip-rule="evenodd">
+                            <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z"
+                                  opacity=".2"/>
+                            <path
+                                d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"/>
+                        </g>
+                    </svg>
                 </div>
             )}
         </button>
